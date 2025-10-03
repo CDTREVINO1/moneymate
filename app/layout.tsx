df49d30bc9eb32053646f7802bc8abcbdbec7f9e
CDTREVINO1 <cdtrevino1@outlook.com>
2025-10-03 13:13:53 -0500

    Add next-themes and dependencies. Add ThemeToggle component. Remove boilerplate from home page. Add button and dropdown-menu from shadcn.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Oxanium, Source_Code_Pro } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const oxaniumSans = Oxanium({
   variable: "--font-oxanium-sans",
@@ -23,11 +24,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${oxaniumSans.variable} ${sourceMono.variable} antialiased font-sans`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableColorScheme
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
