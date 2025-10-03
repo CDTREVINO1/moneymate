c787374319b3c7c35725dc299ff0703730229e82
CDTREVINO1 <cdtrevino1@outlook.com>
2025-10-03 12:13:53 -0500

    Install shadcn, tweakcn theme, and google fonts.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Oxanium, Source_Code_Pro } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const oxaniumSans = Oxanium({
+  variable: "--font-oxanium-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const sourceMono = Source_Code_Pro({
+  variable: "--font-source-code-pro-mono",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${oxaniumSans.variable} ${sourceMono.variable} antialiased font-sans`}
       >
         {children}
       </body>
