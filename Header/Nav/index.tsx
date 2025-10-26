196df93daf99fd170c8d4b1268adf2a08a076284
CDTREVINO1 <cdtrevino1@outlook.com>
2025-10-25 20:57:47 -0500

    Install shadcn sheet for mobile nav menu. Add mobile nav menu.

--- Header/Nav/index.tsx
+++ Header/Nav/index.tsx
@@ -3,11 +3,21 @@
 import React, { Fragment } from "react";
 import Link from "next/link";
 import type { Header as HeaderType } from "@/payload-types";
-import { SearchIcon } from "lucide-react";
+import { SearchIcon, Menu } from "lucide-react";
 
 import { ThemeToggle } from "@/components/ui/ThemeToggle";
 import { CMSLink } from "@/components/Link";
 import { Button } from "@/components/ui/button";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 import { useAuth } from "@/app/(app)/_providers/Auth";
 import type { User } from "@/payload-types.ts";
@@ -16,8 +26,9 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const { user, permissions } = useAuth<User>();
   const navItems = data?.navItems || [];
 
-    return (
-      <nav className="flex items-center gap-3">
+  return (
+    <nav className="flex items-center justify-between">
+      <div className="hidden md:flex items-center gap-3">
         {navItems.map(({ link }, i) => {
           return <CMSLink key={i} {...link} appearance="link" />;
         })}
@@ -51,6 +62,65 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
             <SearchIcon className="w-5 text-primary" />
           </Link>
         </Button>
-      </nav>
-    );
+      </div>
+
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right" className="w-64">
+            <SheetHeader>
+              <SheetTitle className="text-lg font-semibold">Menu</SheetTitle>
+              <SheetDescription>
+                What would you like to do next?
+              </SheetDescription>
+            </SheetHeader>
+            <div className="mt-4 flex flex-col space-y-4 px-6">
+              {navItems.map(({ link }, i) => {
+                return (
+                  <SheetClose key={i} asChild>
+                    <CMSLink key={i} {...link} appearance="link" />
+                  </SheetClose>
+                );
+              })}
+
+              {!user ? (
+                <SheetClose asChild>
+                  <Button asChild>
+                    <Link href="/login">Login</Link>
+                  </Button>
+                </SheetClose>
+              ) : (
+                <Fragment>
+                  <SheetClose asChild>
+                    <Button variant="outline" asChild>
+                      <Link href="/account">{user.email}</Link>
+                    </Button>
+                  </SheetClose>
+
+                  <SheetClose asChild>
+                    <Button asChild>
+                      <Link href="/logout">Logout</Link>
+                    </Button>
+                  </SheetClose>
+                </Fragment>
+              )}
+              {user && permissions?.canAccessAdmin && (
+                <Fragment>
+                  <SheetClose asChild>
+                    <Button asChild>
+                      <Link href="/admin">Admin</Link>
+                    </Button>
+                  </SheetClose>
+                </Fragment>
+              )}
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </nav>
+  );
 };
